1ccf300b660303ad2ccf286bcc0db3bbe444df13
RedGuy12 <paul@reid-family.org>
2021-04-27 11:29:57 -0500

    updates

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.getElementById("nav").innerHTML =
+document.getElementById("nav").innerHTML             =
 	'<p class="center nav">The Periodic Table of Elements | <a href=" /index.html ">Table</a> / <a href=" / credits.html ">Credits</a> / <a href=" / chemistry.html ">More Chemistry</a></p>';
 document.getElementsByTagName("footer")[0].innerHTML =
 	'<p style = "font-size: 20pt;" > Coded with HTML, CSS, and JavaScript < /p>';
-var pic = "g";
-document.cookie = "test=yes";
+var pic                                              = "g";
+document.cookie                                      = "test=yes";
 document
 	.getElementsByClassName("rdi")[0]
 	.setAttribute("src", "images/redir_".concat(pic, ".jpg"));
@@ -11,8 +11,7 @@ document
 function alertjsOO() {
 	console.log(pic);
 	if (pic == "g") {
-		if (
-			confirm(
+		if (confirm(
 				"The redirect alert is ON. Click 'OK' to keep it on or click 'Cancel' to turn it off.",
 			)
 		) {
@@ -21,8 +20,7 @@ function alertjsOO() {
 		}
 	} else {
 		if (pic == "r") {
-			if (
-				confirm(
+			if (confirm(
 					"The redirect alert is OFF. Click 'OK' to turn it on or click 'Cancel' to keep it off.",
 				)
 			) {
@@ -35,8 +33,7 @@ function alertjsOO() {
 					"'.",
 				),
 			);
-			if (
-				confirm(
+			if (confirm(
 					"The redirect alert is ON. Click 'OK' to keep it on or click 'Cancel' to turn it off.",
 				)
 			) {
@@ -60,8 +57,7 @@ function alertjsOO() {
 }
 
 function alertjs(url) {
-	if (
-		confirm(
+	if (confirm(
 			"Redirecting to " +
 				url.split("/")[0].concat("//", url.split("/")[2]),
 		)
